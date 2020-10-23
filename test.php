6a88a0e7e8003e0a7f477cf86a4ea4fc1c86e0a4
korisu07 <Hallo.Dreamy@gmail.com>
2020-10-23 16:59:37 +0900

    メール送信をやってみたい。

--- test.php
+++ test.php
@@ -1,1 +1,18 @@
-<h1><?php echo 'Hello, WSL!Yeah!'; ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<form action="./test_mail_send.php">
+  <p>送り先</p><input type="text" name="to">
+  <p>件名</p><input type="text" name="title">
+  <p>メッセージ</p><textarea name="message" cols="30" rows="10"></textarea>
+  <p><input type="submit" name="send" value="送信"></p>
+</form>
+
+</body>
+</html>
