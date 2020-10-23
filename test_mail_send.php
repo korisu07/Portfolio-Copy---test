569173ad2704d923fe98c0140a813a3880060c33
korisu07 <Hallo.Dreamy@gmail.com>
2020-10-23 17:38:28 +0900

    使用する関数を変更しました。

--- test_mail_send.php
+++ test_mail_send.php
@@ -1,16 +1,23 @@
-<?php 
-    mb_language("Japanese");
-    mb_internal_encoding("UTF-8");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
 
-    $to = $_POST['to'];
-    $title = $_POST['title'];
-    $message = $_POST['message'];
-    $headers = "From: from@example.com";
+    $to = $_POST['to']; //送信先
+    $title = $_POST['title']; //件名
+    $message = $_POST['message']; //本文
+    $header = "From: $to";
 
-    if(mb_send_mail($to, $title, $message, $headers)){
+    if(mail($to, $title, $message, $header)){
       echo "成功です！";
     }else{
-      echo "失敗です…。";
+      echo "失敗です。";
     }
-
-  ?>
+?>
+</body>
+</html>
