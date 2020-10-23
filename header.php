ddfc02067cd63df635e2429fbe2e35be492981b2
korisu07 <Hallo.Dreamy@gmail.com>
2020-10-23 14:55:00 +0900

    first commit. 試しに分割してみました。

---
+++ header.php
@@ -0,0 +1,27 @@
+<header>
+  <h1><a href="index.html">Y.K.Portfolio</a></h1>
+
+  <!-- ハンバーガーメニューのボタン用 -->
+  <input id="menu" type="checkbox">
+  <label for="menu" class="menu_open"></label>
+  <label for="menu" class="menu_back"></label>
+  <!-- ハンバーガーメニューのボタン用 -->
+
+  <nav>
+    <ul>
+      <li class="sp_top"><a href="index.html">Y.K.Portfolio</a></li>
+      <li>
+          <a id="profile" href="profile.html">Profile</a>
+      </li>
+      <li>
+          <a id="works" href="works.html">Works</a>
+      </li>
+      <li>
+          <a id="art" href="art.html">Art</a>
+      </li>
+      <li>
+          <a id="contact" href="contact.html">Contact</a>
+      </li>
+    </ul>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php include 'head.php'; ?>
+
+<body id="index">
+
+  <div id="container">
+
+    <div id="content">
+
+    <?php include 'header.php'; ?>
+
+      <div class="top_content">
+          <div id="top-preview" class="image-first">
+              <div id="opacity"></div> <!-- opacity 透過用 -->
+          </div> <!-- top-preview -->
+      </div>
+
+    </div> <!-- content -->
+
+  <?php include 'footer.php'; ?>
+
+  </div> <!-- container -->
+
+    <!-- トップページ用のスクリプト -->
+    <script src="js/toppage.js"></script>
+
+</body>
+</html>
