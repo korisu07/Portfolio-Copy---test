1d93af850e9b9f3371f262f1ed0651dc40644995
korisu07 <Hallo.Dreamy@gmail.com>
2020-10-26 23:45:20 +0900

    開くメニューの部分を追加

--- header.php
+++ header.php
@@ -15,9 +15,25 @@
       </li>
       <li>
           <a id="works" href="works.html">Works</a>
+          <div id="side-menu">
+            <span><a href="works.html#web-top">WEB</a></span>
+            <!-- <div class="menu-sub">
+              <span><a href="oyasai.html">おやさい.net</a></span>
+              <span><a href="cafe-cookies.html">Cafe*Cookies</a></span>
+            </div> -->
+            <span><a href="works.html#dtp-top">DTP</a></span>
+            <!-- <div class="menu-sub">
+              <span><a href="kumayoke.html">チラシ</a></span>
+              <span><a href="work_postcard.html">ハガキ</a></span>
+            </div> -->
+          </div>
       </li>
       <li>
           <a id="art" href="art.html">Art</a>
+          <div id="side-menu">
+            <span><a href="ocean.html">イラスト</a></span>
+            <span><a href="art_postcard.html">絵はがき</a></span>
+          </div>
       </li>
       <li>
           <a id="contact" href="contact.html">Contact</a>
