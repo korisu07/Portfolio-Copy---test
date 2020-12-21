375df3607a75ad5e929e1eb15a5d987efbd066a1
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-21 23:36:27 +0900

    first commit. コピーページを作成。

---
+++ art.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php include './inc/head.php'; ?>
+
+<body id="art-page">
+
+<div id="container">
+
+  <div id="content">
+
+  <?php include './inc/header.php'; ?>
+
+    <section>
+      <h2>Art</h2>
+      <h3 id="postcard-top">-デジタルイラスト-</h3>
+      <div class="section-box thumbnail_sort">
+        <div class="select_thumb small_thumb">
+          <span>深海</span>
+          <a href="ocean.php"><img src="img/ocean_thumb.jpg" alt="深海" class="img-thumbnail"></a>
+        </div>
+      </div> <!-- /.section-box .thumbnail_sort --> 
+
+      <h3 id="postcard-top">-手書きイラスト-</h3>
+      <div class="section-box thumbnail_sort">
+        <div class="select_thumb small_thumb">
+          <span>絵はがき</span>
+          <a href="art_postcard.php"><img src="img/art_postcard_thumb.jpg" class="img-thumbnail"></a>
+        </div>
+
+      </div> <!-- /.section-box .thumbnail_sort --> 
+
+    </section>
+
+  </div> <!-- content -->
+
+  <?php include './inc/footer.php'; ?>
+
+</div> <!-- container -->
+
+</body>
+</html>
---
+++ ocean.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <?php include './inc/head.php'; ?>
+
+<body id="art-page">
+
+<div id="container">
+
+  <div id="content">
+
+  <?php include './inc/header.php'; ?>
+
+  <section>
+    <h2>Art</h2>
+    <h3>《深海》</h3>
+    <div class="section-box">
+      <div class="preview-box">
+        <div class="preview-img">
+          <a href="img/ocean.png" id="open_img">
+            <img src="img/ocean_preview.png" alt="深海" class="img-thumbnail">
+          </a>
+        </div>
+        <div class="preview-text">
+          <p>制作時間：6時間</p>
+            
+          <p>使用ソフト：Photoshop</p>
+          
+          <p>マウスだけを使用し、どこまで深海が表現できるかチャレンジした作品です。</p>
+        </div>
+      </div> <!-- /.preview-box -->
+    </div> <!-- section-box -->
+        
+  </section>
+  </div> <!-- content -->
+
+  <?php include './inc/footer.php'; ?>
+
+</div> <!-- container -->
+
+
+<script src="js/modal_img.js"></script>
+
+</body>
+</html>
