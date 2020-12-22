438336aaa4a422f4fd00d32047961d560d3180f4
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-22 12:49:23 +0900

    読み込むCSSの都合上、構成を変更。

--- inc/head.php
+++ inc/head.php
@@ -1,19 +1,15 @@
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="ポートフォリオ（作品集）です。">
-    <meta name="keywords" content="Portfolio,ポートフォリオ,Web制作,自主制作,個人制作">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="ポートフォリオ（作品集）です。">
+<meta name="keywords" content="Portfolio,ポートフォリオ,Web制作,自主制作,個人制作">
 
-    <title>Y.K.Portfolio</title>
+<title>Y.K.Portfolio</title>
 
-    <link href="css/cssreset-min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style-sub.css" rel="stylesheet">
-    <link href="css/menu.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/style-sub.css" rel="stylesheet">
+<link href="css/menu.css" rel="stylesheet">
 
-    <script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
 
-    <script src="js/script.js"></script>
-
-</head>
+<script src="js/script.js"></script>
