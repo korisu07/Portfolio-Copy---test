16ad012ad244ba7d739c365d3e716a04dd1f69ec
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-23 19:16:52 +0900

    リンクを修正。APPのリンクを追加

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <header>
-  <h1><a href="index.php">Y.K.Portfolio</a></h1>
+  <h1><a href="/index.php">Y.K.Portfolio</a></h1>
 
   <!-- ハンバーガーメニューのボタン用 -->
   <input id="menu" type="checkbox">
@@ -9,32 +9,37 @@
 
   <nav>
     <ul>
-      <li class="sp_top"><a href="index.php">Y.K.Portfolio</a></li>
+      <li class="sp_top"><a href="/index.php">Y.K.Portfolio</a></li>
       <li>
-          <a id="profile" href="profile.php">Profile</a>
+          <a id="profile" href="/profile.php">Profile</a>
       </li>
       <li>
           <div id="works" class="open_menu">Works</div>
           <div class="side-menu">
-            <a href="works.php">作品一覧</a>
+            <a href="/works.php">作品一覧</a>
+            <a href="#">APP</a>
+            <div class="menu-sub">
+              <span><a href="/chat.php">チャット</a></span>
+              <span><a href="/memo.php">Memo.app</a></span>
+            </div>
             <a href="#">WEB</a>
             <div class="menu-sub">
-              <span><a href="oyasai.php">おやさい.net</a></span>
-              <span><a href="cafe-cookies.php">Cafe*Cookies</a></span>
+              <span><a href="/oyasai.php">おやさい.net</a></span>
+              <span><a href="/cafe-cookies.php">Cafe*Cookies</a></span>
             </div>
             <a href="#">DTP</a>
             <div class="menu-sub">
-              <span><a href="kumayoke.php">チラシ</a></span>
-              <span><a href="work_postcard.php">ハガキ</a></span>
+              <span><a href="/kumayoke.php">チラシ</a></span>
+              <span><a href="/work_postcard.php">ハガキ</a></span>
             </div>
           </div>
       </li>
       <li>
-          <div id="art" class="open_menu">Art</div>
+          <div id="art-link" class="open_menu">Art</div>
           <div class="side-menu">
-            <a href="art.php">作品一覧</a>
-            <a href="ocean.php">イラスト</a>
-            <a href="art_postcard.php">絵はがき</a>
+            <a href="/art.php">作品一覧</a>
+            <a href="/ocean.php">イラスト</a>
+            <a href="/art_postcard.php">絵はがき</a>
           </div>
       </li>
       <li>
