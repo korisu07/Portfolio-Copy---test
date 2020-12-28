1093c92f3597b4db90eb560509af08e5e916d21f
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-28 21:30:41 +0900

    jQueryから素のJSに処理を書き換え。同じリンクに触れたときにフェードしないように改善。

--- js/toppage.js
+++ js/toppage.js
@@ -1,64 +1,67 @@
+const 
+// 画像切り替え部分を読み込み
+	img_preview = document.getElementById('js-top-preview'),
+// h1を読み込み
+	h1 = document.querySelector('header h1');
 
-//マウスを乗せたら発動
-$('nav a').mouseover(function () {
+// idを読み込む関数
+function load_id(id){
+	return document.getElementById(id);
+};
 
-    //一度フェードアウトさせてdivを消す。リセット用にクラスも消す
-    $('div#top-preview').hide().removeClass();
+// 画像切り替えの処理
+function thumb_fadeIn_preview(img_url) {
+
+	const
+	// 現在の背景画像のURLを取得
+		preview_bg = getComputedStyle(img_preview, null).backgroundImage,
+
+	// 一番最後の「/（スラッシュ）」までの文字数をカウント
+		index = preview_bg.lastIndexOf('/', preview_bg.length) + 1,
+
+	// パス部分だけを切り出し。出力例：/img/top.png
+		check_url = '/img/' + preview_bg.replace(/\"\)/, '').substr(index),
+		mouseover_url = img_url;
+		
+
+	// 違うリンクに触れた場合のみ、フェードインが発動。
+	if(check_url != mouseover_url){
+		// 背景画像の切り替え
+		img_preview.style.backgroundImage = `url("${img_url}")`;
+
+		// フェードが発動
+		img_preview.animate({
+			opacity:[0.85, 0]
+		}, 200);
+	
+		img_preview.animate({
+			opacity:[0, 0.85]
+		}, 500);
+	}
+
+}
+
+// h1をマウスオーバー
+h1.addEventListener('mouseover', function(){
+	thumb_fadeIn_preview('/img/top.png');
 });
 
-//マウスを外したときに発動
-$('nav a').mouseout(function () {
-
-    //divを表示しておく
-    $('div#top-preview').show();
+// profileをマウスオーバー
+load_id('profile').addEventListener('mouseover', function(){
+	thumb_fadeIn_preview('/img/profile.png');
 });
 
-$(function () {
+// worksをマウスオーバー
+load_id('works').addEventListener('mouseover', function(){
+	thumb_fadeIn_preview('/img/works.png');
+});
 
-    //  ------------- Top -------------
+// artをマウスオーバー
+load_id('art').addEventListener('mouseover', function(){
+	thumb_fadeIn_preview('/img/art.png');
+});
 
-    //マウスを乗せたら発動
-    $('h1 a').mouseover(function () {
-        //一度フェードアウトさせてdivを消す。リセット用にクラスも消す
-        $('div#top-preview').hide().removeClass();
-
-        //フェードイン処理＋クラス付与して背景が変わる。
-        $('div#top-preview').fadeIn(400).addClass('image-first');
-    });
-
-    //  ------------- Profile -------------
-
-    //マウスを乗せたら発動
-    $('header #profile').mouseover(function () {
-
-        //フェードイン処理＋クラス付与して背景が変わる。
-        $('div#top-preview').fadeIn(400).addClass('image-profile');
-    });
-
-    //  ------------- Works -------------
-
-    //マウスを乗せたら発動
-    $('header #works').mouseover(function () {
-
-        //フェードイン処理＋クラス付与して背景が変わる
-        $('div#top-preview').fadeIn(400).addClass('image-works');
-    });
-
-    //  ------------- Art -------------
-
-    //マウスを乗せたら発動
-    $('header #art').mouseover(function () {
-
-        //フェードイン処理＋クラス付与して背景が変わる
-        $('div#top-preview').fadeIn(400).addClass('image-art');
-    });
-
-    //  ------------- Contact -------------
-
-    //マウスを乗せたら発動
-    $('header #contact').mouseover(function () {
-
-        //フェードイン処理＋クラス付与して背景が変わる
-        $('div#top-preview').fadeIn(400).addClass('image-contact');
-    });
+// contactをマウスオーバー
+load_id('contact').addEventListener('mouseover', function(){
+	thumb_fadeIn_preview('/img/contact.png');
 });
