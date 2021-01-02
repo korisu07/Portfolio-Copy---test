5dfaf39d2755f52795acb2a882a7e5e86a350c1c
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-02 13:17:10 +0900

    処理を整理。専用のクラスが付与されていないときにはdivが追加されないよう改善。

--- js/modal_img.js
+++ js/modal_img.js
@@ -1,59 +1,72 @@
-const 
-imgArea = document.querySelectorAll('.open_img'),
-divContainer = document.querySelector('.container');
+
 
 const 
-tag = `
-  <div id="overlay"></div>
-  <img id="modal_img">
-  `;
+imgArea = Array.from( document.getElementsByClassName('open_img') ),
+divContainer = document.querySelector('.container.main-bg-img');
 
-divContainer.insertAdjacentHTML ('afterend',tag);
+if(imgArea.length >= 1){
+  const 
+    add_img_tag = `
+      <div id="overlay">
+        <img id="modal_img">
+      </div>
+      `;
 
-//画像を表示するdivを取得
-const 
-overlay = document.getElementById("overlay"),
-previewImg = document.getElementById('modal_img');
+  // containerの後ろにタグを追加
+  divContainer.insertAdjacentHTML('afterend',add_img_tag);
+  
+  //画像を表示するdivを取得
+  const 
+    overlay = document.getElementById("overlay"),
+    previewImg = document.getElementById('modal_img');
 
-if(document.documentElement.clientWidth > 425){
+  // 追加したタグを非表示に
+  display_to_none(overlay);
 
-  imgArea.forEach(function(btn){
-    btn.addEventListener('click', function (event){
-      event.preventDefault();
-    
-      const imgSrc = btn.href;
-    
-      previewImg.setAttribute('src', imgSrc);
-    
-      overlay.style.display = 'block';
-      previewImg.style.display = 'inline-block';
-    
-      overlay.animate({
-        opacity:[0, 0.4]
-      }, 200);
-      previewImg.animate({
-        opacity:[0, 0.4]
-      }, 200);
-    
-    });
-  });
-  
-  overlay.addEventListener('click', function (){
-  
-    overlay.animate({
-      opacity:[0.4, 0]
-    }, 200);
-    previewImg.animate({
-      opacity:[0.4, 0]
-  
-  }, 200);
-  
+  // 画面サイズが425pxを超えたときに発動
+  if(document.documentElement.clientWidth > 425){
+
+    // 画像をクリックした時に、プレビューを表示
+    imgArea.forEach((btn) => {
+      btn.addEventListener('click', function (open_event){
+        open_event.preventDefault();
+        img_fade_in(btn, overlay, previewImg);
+      });
+    }); // end forEach
+
+    overlay.addEventListener('click', function (close_event){
+      close_event.preventDefault();
+
+      img_fade_out(overlay);
+    })
+  } // end if clientWidth > 425px
+} // end if imgArea.length >= 1
+
+// 画像をモーダルで表示するための処理
+function img_fade_in(toggle, obj_of_overlay, obj_of_img_tag){
+
+  // クリックしたリンクのhref属性を取得
+  const 
+    imgSrc = toggle.href;
+
+  // imgタグのsrc属性に、取得したhref属性のリンクを付与
+  obj_of_img_tag.setAttribute('src', imgSrc);
+
+  display_to_default(obj_of_overlay, 200);
+  opacity_0_to_100(obj_of_overlay, 200);
+
+} // end function
+
+
+// モーダルの画像を非表示にする処理
+function img_fade_out(obj_of_overlay){
+
+  opacity_100_to_0(obj_of_overlay, 200);
+
+  // 実際に要素を消す時間をずらす
+  // （見た目が不自然になるため）
   setTimeout(function(){
-    overlay.style.display = 'none';
-    previewImg.style.display = 'none';
+    obj_of_overlay.style.display = 'none';
   }, 150);
-  });
-  
 
-}
-
+} // end function
