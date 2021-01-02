30ada218177a17ff0e77bed1a0329b75298f9593
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-02 13:20:03 +0900

    共通の関数を記述したJSを読み込み。

--- inc/head.php
+++ inc/head.php
@@ -8,8 +8,10 @@
 
 <link rel="icon" href="/img/favicon.ico">
 
-<link href="css/style.css" rel="stylesheet">
-<link href="css/style-sub.css" rel="stylesheet">
-<link href="css/menu.css" rel="stylesheet">
+<link href="/css/style.css" rel="stylesheet">
+<link href="/css/style-sub.css" rel="stylesheet">
+<link href="/css/menu.css" rel="stylesheet">
 
-<link href="css/modal_img.css" rel="stylesheet">
+<link href="/css/modal_img.css" rel="stylesheet">
+
+<script src="/js/function.js"></script>
