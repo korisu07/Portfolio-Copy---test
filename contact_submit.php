305e9a1306be94aa51b88bcdf10f7ad6d5c39c88
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-05 20:44:45 +0900

    有効期限を10分に変更。電話番号が未入力の際に分岐を追加。

--- contact_submit.php
+++ contact_submit.php
@@ -5,10 +5,19 @@
     header('Location: /contact_confirm.php', 307);
 
     // それぞれの項目をCookieに登録
-    setcookie("contact[name]", (string) $_POST['name'], time()+300);
-    setcookie("contact[tel]", (string) $_POST['tel'], time()+300);
-    setcookie("contact[email]", (string) $_POST['email'], time()+300);
-    setcookie("contact[content]", (string) $_POST['contact_content'], time()+300);
+    // 有効期限は10分
+    setcookie("contact[name]", (string) $_POST['name'], time()+600);
+    setcookie("contact[tel]", (string) $_POST['tel'], time()+600);
+
+    // 電話番号が未入力の場合
+    if( is_null($_POST['tel']) || $_POST['tel'] === '' ){
+      setcookie("contact[tel]", ' ', time()+600);
+      }
+
+    setcookie("contact[email]", (string) $_POST['email'], time()+600);
+    setcookie("contact[content]", (string) $_POST['contact_content'], time()+600);
+
+
   }else{
     // 万が一、POSTメソッドがなかった場合、お問い合わせページに戻る
     echo '不正な操作です。5秒後に、お問い合わせページに戻ります。';
