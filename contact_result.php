8e3d8e48c3034eeda1d890e95272ac5dc3d09359
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-05 20:42:52 +0900

    resultのメッセージまわりを変更

--- contact_result.php
+++ contact_result.php
@@ -5,10 +5,11 @@
     if( !isset($_COOKIE["flagment"] )){
 
       // 重複防止のフラグ。有効期限は5分
+      // このフラグが残っている間は、再投稿できない。
       setcookie("flagment", "1", time()+300);
 
       // ここに送信処理
-
+      
 
     // Cookieを削除
       
@@ -17,29 +18,44 @@
     setcookie("contact[email]", "", time()-300);
     setcookie("contact[content]", "", time()-300);
 
-      $result_message = '送信ありがとうございました。';
+      $result_head = '送信が完了しました。';
+      $result_message = 'ありがとうございました。';
 
-    }else if( !isset($_COOKIE["contact"]) ){
+    }else if( isset($_COOKIE["contact"]) ){
 
-      $result_message = '送信に失敗しました。不正な操作です。';
+      // 連投防止フラグが残っていて、かつCookieがセットされている場合。
+
+      $result_head = '送信に失敗しました。';
+      $result_message = '連投はできません。<br>5分ほど時間をあけて、再度お試しください。';
 
     }else{
-      
-      $result_message = '送信に失敗しました。内容が重複しています。';
+
+      // その他、連投防止フラグが残っている場合。（戻るボタンで押しちゃった場合を想定）
+
+      $result_head = '送信に失敗しました。';
+      $result_message = '内容が重複しているか、不正な操作が行われました。';
 
     }
 
-
-
-  }else if( isset($_POST['submit_no']) ){
+  }else if( isset($_POST['submit_fix']) ){
 
     header('Location: /contact.php', 307);
 
     if( isset($_COOKIE["flagment"]) ){
-      setcookie("contact[name]", "", time()-300);
-      setcookie("contact[tel]", "", time()-300);
-      setcookie("contact[email]", "", time()-300);
-      setcookie("contact[content]", "", time()-300);
+      setcookie("flagment", "", time()-300);
+    }
+
+
+  }else if( isset($_POST['submit_cancel']) ){
+    
+    header('Location: /contact.php', 307);
+
+    setcookie("contact[name]", "", time()-300);
+    setcookie("contact[tel]", "", time()-300);
+    setcookie("contact[email]", "", time()-300);
+    setcookie("contact[content]", "", time()-300);
+
+    if( isset($_COOKIE["flagment"]) ){
       setcookie("flagment", "", time()-300);
     }
 
@@ -60,14 +76,16 @@
       <?php include './inc/header.php'; ?>
 
       <section>
-        <h2>送信結果</h2>
+        <h2><?= $result_head ?></h2>
         <p class="mb30"><?= $result_message ?></p>
-    
+
+        <input type="button" value="お問い合わせページに 戻る" onClick="location.href='/contact.php'">
       </section>
 
     </div> <!-- /.content -->
 
       <?php include './inc/footer.php'; ?>
+      <script src="/js/contact.js"></script>
       
   </div> <!-- /.container -->
 </body>
