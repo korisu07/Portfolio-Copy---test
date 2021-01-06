9b5ea36ec3dafe66f4665b43b4a9d80841bbf390
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-06 20:54:22 +0900

    formのname属性を変更。Cookie名と統一しました。不要な処理を削除。

--- contact_submit.php
+++ contact_submit.php
@@ -7,16 +7,12 @@
     // それぞれの項目をCookieに登録
     // 有効期限は10分
     setcookie("contact[name]", (string) $_POST['name'], time()+600);
-    setcookie("contact[tel]", (string) $_POST['tel'], time()+600);
-
-    // 電話番号が未入力の場合
-    if( is_null($_POST['tel']) || $_POST['tel'] === '' ){
-      setcookie("contact[tel]", ' ', time()+600);
-      }
 
     setcookie("contact[email]", (string) $_POST['email'], time()+600);
-    setcookie("contact[content]", (string) $_POST['contact_content'], time()+600);
 
+    setcookie("contact[tel]", (string) $_POST['tel'], time()+600);
+
+    setcookie("contact[content]", (string) $_POST['content'], time()+600);
 
   }else{
     // 万が一、POSTメソッドがなかった場合、お問い合わせページに戻る
