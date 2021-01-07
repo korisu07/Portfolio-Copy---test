f313c99c6a22cb80ed0ec38c2570e5fe614f7f6c
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-07 15:48:42 +0900

    メール送信の処理を追加。Cookieの追加をわけました。

--- contact_result.php
+++ contact_result.php
@@ -1,6 +1,6 @@
 <?php
+  include dirname(__FILE__) . '/inc/contact_function.php';
 
-// どのボタンを押したかを判定
 // 「送信」を押した場合に発動
   if( isset($_POST['submit_yes']) ){
 
@@ -10,18 +10,24 @@
       // このフラグが残っている間は、再投稿できない。
       setcookie("flagment_time", "1", time()+300);
 
-      // ここに送信処理
-      
+      // メールの送信処理
+      try{
 
-    // Cookieを削除
-      
-    setcookie("contact[name]", "", time()-300);
-    setcookie("contact[tel]", "", time()-300);
-    setcookie("contact[email]", "", time()-300);
-    setcookie("contact[content]", "", time()-300);
+        contact_mail_send();
 
-      $result_head = '送信が完了しました。';
-      $result_message = 'ありがとうございました。';
+        $result_head = '送信が完了しました。';
+        $result_message = 'ありがとうございました。';
+
+      } catch ( Exception $ex ) {
+
+        $result_head = '送信に失敗しました。';
+        $result_message = '通信に失敗しました。時間をあけてお試しください。';
+
+      }
+
+      // Cookieを削除
+      clear_contanct_cookies();
+
 
     }else if( isset($_COOKIE["contact"]) ){
 
@@ -39,7 +45,7 @@
 
     } // end if - 連投判定
 
-  } // end if - どのボタンを押してきたか判定
+  } // end if - 「送信」を押した場合に発動
 ?>
 
 <!DOCTYPE html>
