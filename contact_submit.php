62618de4257864df30599d83f85804d4a89f1251
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-10 15:41:48 +0900

    このページに不意にたどり着いてしまった場合、リダイレクトが働くように処理を追加。

--- contact_submit.php
+++ contact_submit.php
@@ -1,18 +1,48 @@
-<?php 
-  include dirname(__FILE__) . '/inc/contact_function.php';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php include './inc/head.php'; ?>
+</head>
 
-  if( isset($_POST['name']) ){
+<body id="contact-page">
+  <div class="container main-bg-img">
+    <div class="content">
 
-    // 確認画面にリダイレクト
-    header('Location: /contact_confirm.php', 307);
+      <?php include './inc/header.php'; ?>
+  
+    <section>
 
-    // それぞれの項目をCookieに登録
-    // 有効期限は10分
+    <?php 
+      include dirname(__FILE__) . '/inc/contact_function.php';
 
-    set_contanct_cookies($_POST['name'], $_POST['company'], $_POST['tel'], $_POST['email'], $_POST['content']);
+      if( isset($_POST['name']) ){
 
-  }else{
-    // 万が一、POSTメソッドがなかった場合、お問い合わせページに戻る
-    echo '不正な操作です。5秒後に、お問い合わせページに戻ります。';
-    header('Location: /contact.php', 307);
-  }
+        // 確認画面にリダイレクト
+        header('Location: /contact_confirm.php', 307);
+
+        // それぞれの項目をCookieに登録
+        // 有効期限は10分
+
+        set_contanct_cookies($_POST['name'], $_POST['company'], $_POST['tel'], $_POST['email'], $_POST['content']);
+
+      }else{
+        // 万が一、POSTメソッドがなかった場合、お問い合わせページに戻る
+        echo '不正な操作です。5秒後に、お問い合わせページに戻ります。';
+        echo 
+          '<script>
+            setTimeout(function () {
+              window.location.href= "/contact.php";
+            
+            },5000);
+          </script>';
+      }
+    ?>
+
+    </section>
+
+    </div> <!-- /.content -->
+
+  <?php include './inc/footer.php'; ?>
+  </div> <!-- /.container -->
+</body>
+</html>
