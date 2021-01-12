fb6361533b437dd0f79283535aa5c2a66d35fe85
korisu07 <Hallo.Dreamy@gmail.com>
2021-01-12 20:25:41 +0900

    今年の西暦を自動で表示するように変更しました。

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,16 @@
 <footer>
-  2019 - 2020 Y.K.Portfolio.
+  <?php 
+    $space_str = '&nbsp;';
+    // footerを表示
+    // 出力例：2019 - 2021  Y.K.Portfolio.
+    echo 
+      '2019',
+      "${space_str}-${space_str}",
+      date('Y'), //自動で今年の西暦を表示
+      "${space_str}${space_str}",
+      'Y.K.Portfolio.';
+
+    ?>
 </footer>
 
 <script src="js/fadeIn.js"></script>
