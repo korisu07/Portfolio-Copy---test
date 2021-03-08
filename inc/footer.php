9563d69204e2383d8b8f1def2c01d74236e7e0f6
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-08 16:36:56 +0900

    JSの読み込みのパスを統一

--- inc/footer.php
+++ inc/footer.php
@@ -15,7 +15,7 @@
 
 <script src="/js/function.js"></script>
 
-<script src="js/fadeIn.js"></script>
+<script src="/js/fadeIn.js"></script>
 
-<script src="./js/side_menu_open.js"></script>
+<script src="/js/side_menu_open.js"></script>
 <script src="/js/modal_img.js"></script>
