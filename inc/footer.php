5bd8c24d90fd10c2f048f5c9124a0c5da25a2aa1
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-26 19:30:41 +0900

    footerの表記を簡潔にしました。

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,5 @@
 <footer>
-  <?php 
-    $space_str = '&nbsp;';
-    // footerを表示
-    // 出力例：2019 - 2021  Y.K.Portfolio.
-    echo 
-      '2019',
-      "${space_str}-${space_str}",
-      date('Y'), //自動で今年の西暦を表示
-      "${space_str}${space_str}",
-      'Y.K.Portfolio.';
-
-    ?>
+  2019 - <?= date('Y') //今年の西暦を表示 ?> Y.K.Portfolio.
 </footer>
 
 <script src="/js/function.js"></script>
